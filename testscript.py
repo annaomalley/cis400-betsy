31ca4e6aedb5bfc92efd24b607ba6d4878156918
Dylan-Brown <brodylan@sas.upenn.edu>
2017-11-20 22:23:59 -0500

    updated testscript; access  issue seems to be serverside

--- testscript.py
+++ testscript.py
@@ -1,6 +1,24 @@
+import sys
 import pymongo
 from pymongo import MongoClient
 
-HOST = 'ec2-54-172-179-228.compute-1.amazonaws.com'
-PORT = 27017
-CLIENT = MongoClient(HOST, PORT)
+USER = 'test'
+PWD = 'testpassword'
+PORT = '27017'
+HOST_PUB_IP = '54.172.179.228' #HOST_PRIV_IP = '172-31-49-21'
+HOST = 'mongodb://'+USER+':'+PWD+'@'+HOST_PUB_IP+':'+PORT
+
+# Attempt to establish connection with a 3s timeout.
+def get_client(host):
+    try:
+        CLIENT = MongoClient(host, serverSelectionTimeoutMS=3000, connectTimeoutMS=20000)
+        print '\t' + 'attempting to establish connection to client:'
+        print '\t' + str('host: ' + HOST)
+        CLIENT.server_info()
+        return CLIENT
+    except pymongo.errors.ServerSelectionTimeoutError as err:
+        print '\t' + 'ERR: Connection timed out. ' + str(err)
+        sys.exit(0)
+
+CLIENT = get_client(HOST)
+CLIENT.close()
