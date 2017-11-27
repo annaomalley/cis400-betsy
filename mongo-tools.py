bcc87b7444a4cb9b541fa2eea94fc82f2cf0ed14
Dylan-Brown <brodylan@sas.upenn.edu>
2017-11-27 11:46:11 -0500

    made it even easier to push data to the db

--- mongo-tools.py
+++ mongo-tools.py
@@ -1,6 +1,6 @@
 import sys
-import pymongo
 import json
+import pymongo
 from pymongo import MongoClient
 
 # Attempt to establish connection with a 5s timeout.
@@ -16,15 +16,15 @@ def get_client(uname, password):
         print '\t' + 'ERR: Connection timed out. ' + str(err)
         sys.exit(0)
 
-def insert_collection(client, connection, record):
-    new_collection = client[name]
-    post_id = new_collection.insert_one(record).inserted_id
-    print '\t...inserted into collection [' + name + ']: ' + str(post_id)
+def insert_collection(client, collection_name, record):
+    collection = client[collection_name,]
+    post_id = collection.insert_one(record).inserted_id
+    #print '\t...inserted into collection [' + name + ']: ' + str(post_id)
 
 def create_collection(client, name):
     new_collection = client[name]
     post_id = new_collection.insert_one({"init":1}).inserted_id
-    print '\t...new collection [' + name + ']: ' + str(post_id)
+    #print '\t...new collection [' + name + ']: ' + str(post_id)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
--- scrape.py
+++ scrape.py
@@ -1,6 +1,8 @@
 import sys
+import json
 import urllib2                 # https://docs.python.org/2/library/urllib2.html
 from bs4 import BeautifulSoup  # https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+from mongo-tools import get_client, create_collection, insert_collection
 
 # Returns a list of URLs to scrape. File is assumed to be a list of newline seperated URLs
 def get_urls_from_file(path):
@@ -21,14 +23,26 @@ def scrape(urls):
         page_data[url] = con.read()
     return page_data
 
-# Given the page data from squawka, parse the relevant information
+# Given the page data from squawka, parse the relevant information.
 def parse_squawka(data):
+    # TODO: implement
     pass
 
+# Given a list of JSON objects, push them to the mongo database in the specified collection
+def push_to_db(data, uname, password, collection_name):
+    client = get_client(uname, password)
+    create_collection(client, collection_name)
+    for d in data: # each should be a JSON object
+        insert_collection(client, collection_name, d)
+
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print 'To run this script, run python scrape.py <file_containing_urls>'
+    if len(sys.argv) != 4:
+        print 'To run this script, run python scrape.py <file_containing_urls> <mongo username> <mongo password>'
         sys.exit()
     path = sys.argv[1]
+    uname = sys.argv[2]
+    password = sys.argv[3]
     urls = get_urls_from_file(path)
-    scrape(urls)
+    data = scrape(urls)
+    squawka_data = parse_squawka(data)
+    #push_to_db
