a8741ef7a254fa79167e479a63afaf383fdafe80
Dylan-Brown <brodylan@sas.upenn.edu>
2018-02-21 18:22:52 -0500

    limited team pages; game page

--- website/Server.js
+++ website/Server.js
@@ -12,12 +12,24 @@ router.get("/",function(req,res){
   res.sendFile(path + "index.html");
 });
 
+router.get("/home",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
 router.get("/about",function(req,res){
   res.sendFile(path + "about.html");
 });
 
+router.get("/game",function(req,res){
+  res.sendFile(path + "game.html");
+});
+
 router.get("/contact",function(req,res){
   res.sendFile(path + "contact.html");
+});
+
+router.get("/team",function(req,res){
+  res.sendFile(path + "team.html");
 });
 
 app.use("/",router);
@@ -29,4 +41,3 @@ app.use("*",function(req,res){
 app.listen(3000,function(){
   console.log("Live at Port 3000");
 });
-
